b1b96f48c40515c559d36a51fa2c5e146585b358
zcesar <cesar.mac26@gmail.com>
2022-06-25 00:09:38 -0500

    A5_Lagrange
    Actividad 5 "caso práctico"

---
+++ A5_lagrange.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Método de lagrange
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+from codigo.metodos_numericos.lagrange import LagrangePol
+
+
+
+"""
+    Entradas:
+    Datos -- lista de puntos (x,y) de velocidad del paracaidista
+             ------------------------------------
+             Tiempo (s)       Velocidad (cm/s)
+             ------------------------------------
+                1               800 
+                3              2310
+                5              3090
+                7              3940
+               13              4755
+               
+   Estimación en t=10 
+   
+"""
+Datos = [(1, 800), (3, 2310), (5, 3090), (7,3940), (13, 4755)]
+t = 10
+
+#Función
+Pf = LagrangePol(Datos)
+
+#Obtener valor 
+Pf_t10= Pf(t)
+print("\n\n El valor del polinomio de Lagrange en t = 10:")
+print(" {0:.8f}".format(Pf_t10))
+
+# se agrega el nuevo valor para t=10 a Datos
+Datos.append((t,Pf_t10))
+
+#Puntos para la grafica
+a = [val[0] for val in Datos]
+b = [val[1] for val in Datos]
+
+#Puntos para el polinoimio
+n=60
+x_pol = np.linspace(min(a), max(a), n)
+y_pol = [ Pf(i) for i in x_pol]
+
+
+
+# Grafica
+plt.plot(a,b,'o',label = 'Puntos')
+plt.plot(x_pol,y_pol, label = 'Polinomio')
+plt.legend( loc='lower center')
+
+
+
---
+++ codigo/metodos_numericos/lagrange.py
@@ -0,0 +1,24 @@
+# Método de Lagrange
+
+def LagrangePol(datos):
+
+    def f_Lagrange(k, x):
+        """Implementación funciones L_k(x)"""
+        out = 1
+        for i, p in enumerate(datos):
+            if i != k:
+                out *= (x - p[0])/(datos[k][0] - p[0])        
+        return out
+
+    def P(x):
+        
+        """Implementación polinomio P(x)"""
+        # polinomio P(x) = Pkf(xk)Lk(x)
+        lag = 0
+        for k, p in enumerate(datos):
+            lag += p[1]*f_Lagrange(k, x)
+            
+        return lag
+ 
+           
+    return P
